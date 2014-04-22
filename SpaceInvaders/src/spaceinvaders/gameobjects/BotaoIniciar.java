4c435fe5070744cb28ef3a1d33829ec97fc4009f
Magally <jmagally.ecomp@gmail.com>
2014-04-21 23:24:15 -0300

    Menu do Jogo: botao para iniciar o jogo(erro ao apertar enter)

--- SpaceInvaders/src/spaceinvaders/gameobjects/BotaoIniciar.java
+++ SpaceInvaders/src/spaceinvaders/gameobjects/BotaoIniciar.java
@@ -20,62 +20,19 @@ import spaceinvaders.utils.sprite.SpriteAnimated;
  * @author Jéssica Magally
  */
 public class BotaoIniciar extends GameObject {
+    SpaceInvaders spInvaders;
+    
+    public BotaoIniciar(SpaceInvaders spInvaders){
+        spInvaders= new SpaceInvaders();
+    }
 
     @Override
     public void update() {
         if(keyPressed== KeyEvent.VK_ENTER){
-             this.iniciarJogo();
+            spInvaders.iniciarJogo();
+             
         }
     }
-
-   
     
-    
-    public void iniciarJogo() {
-        Jogo jogo = new Jogo();
-
-        GameObject nave = new Nave();
-
-        ///Adicionando nave
-        SpriteAnimated spNave;
-        spNave = new SpriteAnimated();
-        try {
-            spNave.carregarSprite("src/assets/nave1.png", 1, 2);
-            spNave.setX(30);
-            spNave.setY(300);
-            spNave.animate(0,1);
-        } catch (Exception ex) {
-            Logger.getLogger(SpaceInvaders.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        nave.setSprite(spNave);
-
-        jogo.addGameObject(nave);
-        //Adicionando inimigo
-        Inimigo inimigo = new Inimigo();
-        SpriteAnimated spInimigo = new SpriteAnimated();
-        try {
-            spInimigo.carregarSprite("src/assets_800x600/alien3.png", 1, 4);
-            spInimigo.setX(10);
-            spInimigo.setY(20);
-            spInimigo.animate(0, 3);
-        } catch (Exception ex) {
-            Logger.getLogger(SpaceInvaders.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        inimigo.setSprite(spInimigo);
-        jogo.addGameObject(inimigo);
-
-        BackgroundImage bg = new BackgroundImage("src/assets/background.jpg");
-        try {
-            jogo.addBackground(bg);
-        } catch (Exception ex) {
-            Logger.getLogger(SpaceInvaders.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        jogo.start();
-    }
-
-    
-    
-    
-   
+       
 }
